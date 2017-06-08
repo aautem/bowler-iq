afd5743947130148066d238d8cfd4eb1d3af626e
Alex Autem <autem.alex@gmail.com>
2017-06-07 23:07:26 -0700

    Move Nav from components to containers

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
-import Nav from './../components/Nav';
+import Nav from './Nav';
 import Overview from './Overview';
 import BowlGame from './BowlGame';
 import OldGame from './OldGame';
