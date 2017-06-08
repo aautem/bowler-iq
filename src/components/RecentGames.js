fb1d29d28759df8accd85a7372dcfe28e5c32047
Alex Autem <autem.alex@gmail.com>
2017-06-07 23:22:47 -0700

    Add game sections to Recent Games container on home page

--- src/components/RecentGames.js
+++ src/components/RecentGames.js
@@ -5,7 +5,31 @@ const RecentGames = (props) => {
   return (
     <div className="col s12 m6">
       <div className="grey darken-1 z-depth-5 stat-block">
-        <h4>Recent Games</h4>
+        <div className="row">
+          <div className="col s12">
+            <h4>Recent Games</h4>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col s12 m4">
+            Game 1
+          </div>
+
+          <div className="col s12 m4">
+            Game 2
+          </div>
+
+          <div className="col s12 m4">
+            Game 3
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col s12">
+            <a>More Games</a>
+          </div>
+        </div>
       </div>
     </div>
   );
