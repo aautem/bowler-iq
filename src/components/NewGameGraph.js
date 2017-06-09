7325deb59e9c3d12b876a5b0587a455d1afe0174
Alex Autem <autem.alex@gmail.com>
2017-06-09 15:05:32 -0700

    Create helper function to calculate game max at each frame

--- src/components/NewGameGraph.js
+++ src/components/NewGameGraph.js
@@ -3,6 +3,18 @@ import React from 'react';
 const NewGameGraph = (props) => {
   console.log('NewGameGraph Props:', props);
 
+  var getMax = function(frame) {
+    if (frame === 1) {
+      return 300;
+    } else {
+      if (props.scorecard[frame - 2].totalScore) {
+        return (props.scorecard[frame - 2].totalScore + (30 * (11 - frame)));
+      } else {
+        return getMax(frame - 1);
+      }
+    }
+  };
+
   // set up Google Chart
   window.google.charts.load('current', {'packages':['corechart']});
   window.google.charts.setOnLoadCallback(drawChart);
@@ -10,16 +22,16 @@ const NewGameGraph = (props) => {
   function drawChart() {
     var data = window.google.visualization.arrayToDataTable([
       ['Frame', 'Total Score', 'Max Score'],
-      ['1', (props.scorecard[0].totalScore || 0), 300],
-      ['2', props.scorecard[1].totalScore, (props.scorecard[0].totalScore + (30 * 9))],
-      ['3', props.scorecard[2].totalScore, (props.scorecard[1].totalScore + (30 * 8))],
-      ['4', props.scorecard[3].totalScore, (props.scorecard[2].totalScore + (30 * 7))],
-      ['5', props.scorecard[4].totalScore, (props.scorecard[3].totalScore + (30 * 6))],
-      ['6', props.scorecard[5].totalScore, (props.scorecard[4].totalScore + (30 * 5))],
-      ['7', props.scorecard[6].totalScore, (props.scorecard[5].totalScore + (30 * 4))],
-      ['8', props.scorecard[7].totalScore, (props.scorecard[6].totalScore + (30 * 3))],
-      ['9', props.scorecard[8].totalScore, (props.scorecard[7].totalScore + (30 * 2))],
-      ['10', props.scorecard[9].totalScore, (props.scorecard[8].totalScore + 30)]
+      ['1', props.scorecard[0].totalScore, getMax(1)],
+      ['2', props.scorecard[1].totalScore, getMax(2)],
+      ['3', props.scorecard[2].totalScore, getMax(3)],
+      ['4', props.scorecard[3].totalScore, getMax(4)],
+      ['5', props.scorecard[4].totalScore, getMax(5)],
+      ['6', props.scorecard[5].totalScore, getMax(6)],
+      ['7', props.scorecard[6].totalScore, getMax(7)],
+      ['8', props.scorecard[7].totalScore, getMax(8)],
+      ['9', props.scorecard[8].totalScore, getMax(9)],
+      ['10', props.scorecard[9].totalScore, getMax(10)]
     ]);
 
     var options = {
