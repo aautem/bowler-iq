62839059c822143d9db84d595f696591663ff64b
Alex Autem <autem.alex@gmail.com>
2017-06-12 22:19:58 -0700

    Sort game history by date when retrieving from db

--- react-ui/src/actions/gameHistoryActions.js
+++ react-ui/src/actions/gameHistoryActions.js
@@ -8,9 +8,33 @@ export function loadGames(userId) {
     axios.get('/api/users/' + userId + '/games')
       .then(function(response) {
         console.log('Game History Response:', response.data);
+
+        // sort game history by date
+        var gameHistory = response.data;
+
+        // return b.date - a.date
+        gameHistory.sort(function(a, b) {
+          var aYear = parseInt(a.date.slice(0, 4));
+          var bYear = parseInt(b.date.slice(0, 4));
+
+          var aMonth = parseInt(a.date.slice(5, 7));
+          var bMonth = parseInt(b.date.slice(5, 7));
+
+          var aDay = parseInt(a.date.slice(8));
+          var bDay = parseInt(b.date.slice(8));
+
+          if (aYear !== bYear) {
+            return bYear - aYear;
+          } else if (aMonth !== bMonth) {
+            return bMonth - aMonth;
+          } else {
+            return bDay - aDay;
+          }
+        });
+
         dispatch({
           type: 'LOAD_GAMES',
-          payload: response.data
+          payload: gameHistory
         });
       })
       .then(function() {
