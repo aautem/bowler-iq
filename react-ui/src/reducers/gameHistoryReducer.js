62fb6f25d4281e4de0f4a9ba446c004d7b84abdd
Alex Autem <autem.alex@gmail.com>
2017-06-12 22:21:49 -0700

    Sort game history array by date on each addition

--- react-ui/src/reducers/gameHistoryReducer.js
+++ react-ui/src/reducers/gameHistoryReducer.js
@@ -7,7 +7,28 @@ export default function reducer(state = [], action) {
   if (action.type === 'UPDATE_GAME_HISTORY') {
     // ADD NEW GAME TO ARRAY AND SORT BY DATE
     var gameHistory = state.slice();
-    gameHistory.unshift(action.payload);
+    gameHistory.push(action.payload);
+
+    // return b.date - a.date
+    gameHistory.sort(function(a, b) {
+      var aYear = parseInt(a.date.slice(0, 4));
+      var bYear = parseInt(b.date.slice(0, 4));
+
+      var aMonth = parseInt(a.date.slice(5, 7));
+      var bMonth = parseInt(b.date.slice(5, 7));
+
+      var aDay = parseInt(a.date.slice(8));
+      var bDay = parseInt(b.date.slice(8));
+
+      if (aYear !== bYear) {
+        return bYear - aYear;
+      } else if (aMonth !== bMonth) {
+        return bMonth - aMonth;
+      } else {
+        return bDay - aDay;
+      }
+    });
+
     return gameHistory;
   }
   // if no match above
