b6518d9738cd8f3461d2e1211c0faf678a7d7f84
Alex Autem <autem.alex@gmail.com>
2017-06-15 22:42:20 -0700

    Move component tests into component folder

--- react-ui/src/components/tests/Nav.test.js
+++ react-ui/src/components/tests/Nav.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Nav from './../components/Nav';
+import Nav from './../Nav';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
